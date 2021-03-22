cc4196f6ae82755ad8accd749e7dd6f549d7e573
Fabrice Robini <frobini@change.org>
2021-03-21 21:11:55 -0700

    [Petitions] Seeding 1 petition

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Petition.create([{ starter_urn: 'urn:changeorg:starter:6757e68c-8ac4-11eb-8dcd-0242ac130003', title: 'Save our oceans', body: 'Lorem ipsum...' }])
