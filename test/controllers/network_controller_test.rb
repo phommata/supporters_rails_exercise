5b9b4c80eb2076a8912d84bf3858f8bc907ece25
Karl Varga <kjvarga@gmail.com>
2021-03-23 09:29:19 -0700

    Bootstrap rspec tests

--- test/controllers/network_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class NetworkControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get network_index_url
-    assert_response :success
-  end
-end
