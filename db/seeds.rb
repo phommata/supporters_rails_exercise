be9414410497248ea93bb446722a9cf2f308c56b
Fabrice Robini <frobini@change.org>
2021-03-24 12:48:17 -0700

    [Seed] Adding more petitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Petition.create([{ starter_urn: 'urn:changeorg:starter:6757e68c-8ac4-11eb-8dcd-0242ac130003', title: 'Save our oceans', body: 'Lorem ipsum...' }])
+Petition.create([{ starter_urn: 'urn:changeorg:starter:c6e83ee8-151b-4ac0-8e4a-b0dcc11a7f0d', title: 'Fight for justice', body: 'Lorem ipsum...' }])
+Petition.create([{ starter_urn: 'urn:changeorg:starter:f5ae944c-8cd8-11eb-8dcd-0242ac130003', title: 'Sign for climate change', body: 'Lorem ipsum...' }])
+Petition.create([{ starter_urn: 'urn:changeorg:starter:6757e68c-8ac4-11eb-8dcd-0242ac130003', title: 'Ban the Cruel Sale & Trade of Shark Fins', body: 'Lorem ipsum...' }])
+Petition.create([{ starter_urn: 'urn:changeorg:starter:ad922736-8cd9-11eb-8dcd-0242ac130003', title: 'Seatbelts for Canadian School Buses, Now!', body: 'Lorem ipsum...' }])
+Petition.create([{ starter_urn: 'urn:changeorg:starter:c6e83ee8-151b-4ac0-8e4a-b0dcc11a7f0d', title: 'COVID-19 funding for Indigenous communities', body: 'Lorem ipsum...' }])
+Petition.create([{ starter_urn: 'urn:changeorg:starter:b1da6920-8cd9-11eb-8dcd-0242ac130003', title: 'Safer Personal Protective Equipment (PPE) for NHS workers', body: 'Lorem ipsum...' }])
+Petition.create([{ starter_urn: 'urn:changeorg:starter:b5f3eebe-8cd9-11eb-8dcd-0242ac130003 ', title: 'Cancer patients exempted from travel restrictions', body: 'Lorem ipsum...' }])
