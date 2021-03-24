6f7fc427d4a723b63befa9b1e91e71e8d64bd221
Fabrice Robini <frobini@change.org>
2021-03-24 13:08:46 -0700

    [Mocks] Adding Petitions mock and moving mocks to dedicated directory

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -1,6 +1,6 @@
 class NetworkController < ApplicationController
 
   def index
-  	render json: File.read("./app/network-mock.json")
+  	render json: File.read("./app/mocks/network-mock.json")
   end
 end
